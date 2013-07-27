2b44bb9c9064dd882b913a47818facbec71a3535
Brian To <bxt5647@rit.edu>
2013-07-26 23:11:30 -0400

    Refactored grid and extracted render_actions

--- app/helpers/teachable_helper.rb
+++ app/helpers/teachable_helper.rb
@@ -1,7 +1,20 @@
 module TeachableHelper
+  # Paths
+  def grid_teachable_path(teachable)
+    # TODO figure out less icky way
+    eval("grid_#{@model_class.to_s.downcase}_path(teachable, :charset => @charset, :transcript => @transcript)")
+  end
+
+  # Renderers
   def render_characters(flash_cards)
     render :partial => "partials/characters", :locals => {
       :flash_cards => flash_cards
+    }
+  end
+
+  def render_actions(teachable)
+    render :partial => "partials/actions", :locals => {
+      :teachable => teachable
     }
   end
 end
