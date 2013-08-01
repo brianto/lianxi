e8131e64603cbe30de8a9d93c68c8e54b1240923
Brian To <bxt5647@rit.edu>
2013-07-31 23:51:54 -0400

    Make assets:precompile not fail randomly
    https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Lianxi
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Heroku magic
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
