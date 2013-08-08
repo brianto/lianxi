0c33c7a63adf4863c8b751aa1be743bf274ba961
Brian To <bxt5647@rit.edu>
2013-08-08 18:12:40 -0400

    Added drill controllers test

--- test/functional/drills_controller_test.rb
+++ test/functional/drills_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class DrillsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "create with no data gives error" do
+    post :create, :drill => { }
+    assert_template :new
+  end
+
+  test "create with full data goes to show" do
+    post :create, :drill => {
+      :title => "title",
+      :description => "description"
+    }
+    assert_redirected_to drill_path(assigns(:drill))
+  end
 end
