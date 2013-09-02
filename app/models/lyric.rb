78db45e9c34a081f40e0c9b96d6796f990c92aad
Brian To <bxt5647@rit.edu>
2013-09-02 03:16:19 -0400

    Added line iterator for lyric

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -3,4 +3,25 @@ class Lyric < ActiveRecord::Base
 
   attr_accessible :dialect, :pronunciation,
     :simplified, :timing, :traditional, :translation
+
+  def lines
+    [ timing.split("\n"),
+      simplified.split("\n"),
+      traditional.split("\n"),
+      pronunciation.split("\n"),
+      translation.split("\n") ].transpose.map do |things|
+        line = Line.new
+        line.timing = things[0]
+        line.simplified = things[1]
+        line.traditional = things[2]
+        line.pronunciation = things[3]
+        line.translation = things[4]
+        line
+    end
+  end
+end
+
+class Line
+  attr_accessor :simplified, :traditional,
+    :pronunciation, :timing, :translation
 end
