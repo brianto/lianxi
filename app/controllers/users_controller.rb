b766f6b0210ad0438e3f6b562cf9b3efb91a5ad2
Brian To <bxt5647@rit.edu>
2014-01-25 08:20:06 -0500

    Fix users signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,14 @@
 class UsersController < ApplicationController
   def new
-    @errors = Hash.new
     @new_user = User.new
   end
 
   def create
     @new_user = User.new user_params
-    @errors = @new_user.errors
 
     if @new_user.save then
       @user = @new_user
-      redirect_to user_path(@new_user)
+      redirect_to user_path(@user)
     else
       render "new"
     end
